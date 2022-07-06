fdc6c5eadb734f79134f4fde980759c65b31e979
Kirill <sonik3000.83@gmail.com>
2022-07-06 17:37:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int bilet = 500; // стоимость билета
+
+        int sum = 20; // Стоимость 1-й бонусной мили
+
+        System.out.println((bilet / sum) + " бонусных миль ");
+
+
+    }
+}
